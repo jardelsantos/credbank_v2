e2ad962193cc60a9c920de0725669d9902a74ce8
Jardel Santos <jardelsilvasantos@hotmail.com>
2023-03-06 22:24:43 -0300

    completo

--- dashboard/resources/views/livewire/extratos.blade.php
+++ dashboard/resources/views/livewire/extratos.blade.php
@@ -44,7 +44,7 @@
                             <button
                                 wire:click="edit({{ $extrato->id }})"
                                 class="bg-blue-500 hover:bg-blue-700
-                                        text-white font-bold py-2 px-4 rounded">Editar</button>
+                                     font-bold py-2 px-4 rounded">Editar</button>
 
                             <button
                                 wire:click="delete({{ $extrato->id }})"
